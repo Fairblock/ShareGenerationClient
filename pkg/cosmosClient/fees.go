656fe016f937ff3e2acd68f6223b6c55e685339f
ap0calypse644 <91111707+ap0calypse644@users.noreply.github.com>
2026-08-31 14:42:51 +0530

    chore: remove unused fee helper

--- pkg/cosmosClient/fees.go
+++
@@ -1,7 +0,0 @@
-package cosmosClient
-
-import "cosmossdk.io/math"
-
-func calculateFeeAmount(gasLimit uint64, gasPrice math.LegacyDec) math.Int {
-	return gasPrice.MulInt(math.NewIntFromUint64(gasLimit)).Ceil().TruncateInt()
-}
